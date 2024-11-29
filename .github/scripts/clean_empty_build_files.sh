aefb4031ef45f6d7e8c5b14a503a212443997f5f
Tim <timothy.dunphy@gmail.com>
2024-11-29 16:36:48 +0000

    feat/exports: Don't echo.

--- .github/scripts/clean_empty_build_files.sh
+++ .github/scripts/clean_empty_build_files.sh
@@ -7,7 +7,6 @@ for file in $build_dir; do
     # Read the first character of the file
     first_char=$(head -c 1 "$file")
 
-    echo $first_char
     if [[ -z "$first_char" || "$first_char" == $'\n' ]]; then
       echo "Deleting empty file: $file"
       rm "$file"
